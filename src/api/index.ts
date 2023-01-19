a544c3ad479405698046ec9175a5d0e23354b0d7
Ermal Vrapi <ermalvrapi18@gmail.com>
2023-01-19 19:10:12 +0100

    fix: remove withCredentials check from api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "https://api.imgur.com/3/gallery",
-    withCredentials: true,
     headers: {
         Authorization: 'Client-ID 29088592405784a',
     },
